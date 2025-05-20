d17362c154b99fb04a7deabb6f59ffa6ab7d7f1d
Expando <xavierroso@Xaviers-MacBook-Air.local>
2025-05-20 17:45:57 +0800

    added a themeContext , theme switch function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import Counter from "../components/counter";
 import Clock from "../components/clock";
+import ThemeSwitcher from "../components/themeSwitcher";
 
 const page = () => {
   return (
     <div className="flex flex-col justify-between">
+      <ThemeSwitcher />
       <Clock />
       <Counter />
     </div>
