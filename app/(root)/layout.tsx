a251cf29921ee4923b0096abc5b883e833b6e696
Expando <xavierroso@Xaviers-MacBook-Air.local>
2025-05-20 17:54:26 +0800

    added a navbar, and navgation function and pages for each links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode } from "react";
+import Navbar from "../components/Navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col">
+      <Navbar />
       <div className="min-h-screen flex items-center justify-center">
         {children}
       </div>
