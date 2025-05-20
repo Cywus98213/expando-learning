de225263ed44770e3daaf4651b1fa97f53441e18
cywus98213@gmail.com <cywus98213@gmail.com>
2025-05-20 22:18:55 +0800

    create a page which SSR to fetch static json, and render the posts

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
       <div className="text-3xl">About</div>
       <div className="flex items-center justify-center">
         <button
-          onClick={() => router.back()}
+          onClick={() => router.push("/")}
           className="border border-gray-300 rounded-md px-2 py-1"
         >
           Go Back
---
+++ app/(root)/posts/page.tsx
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+import PostCard from "@/app/components/PostCard";
+
+const Page = async () => {
+  const filePath = "./public/data.json";
+  const jsonData = await fs.readFile(filePath, "utf8");
+  const data: PostProps[] = JSON.parse(jsonData);
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center">
+      <h1 className="text-4xl font-bold">Posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5 mt-5">
+        {data.map((post) => (
+          <PostCard key={post.id} {...post} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
